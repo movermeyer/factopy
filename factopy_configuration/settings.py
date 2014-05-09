6efbb5ef454251703aed2ba4c389b18380ae0700
eloy <eloy.colell@gmail.com>
2014-05-09 11:52:33 -0300

    It added the django-debug-toolbar.

--- factopy_configuration/settings.py
+++ factopy_configuration/settings.py
@@ -2,6 +2,8 @@
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+INTERNAL_IPS = ('127.0.0.1',)
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
@@ -86,6 +88,7 @@ MIDDLEWARE_CLASSES = (
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
@@ -109,6 +112,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'polymorphic',
     'django.contrib.contenttypes',
     # Uncomment the next line to enable the admin:
--- factopy_configuration/urls.py
+++ factopy_configuration/urls.py
@@ -1,16 +1,19 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
 # Api provide an easy way of automatically determining the URL conf.
 
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    url(r'^factopy/', include('factopy.urls')),
+urlpatterns = \
+    patterns('',
+             url(r'^factopy/', include('factopy.urls')),
+             url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+             url(r'^admin/', include(admin.site.urls)),
+             )
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    url(r'^admin/', include(admin.site.urls)),
-)
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+                            url(r'^__debug__/', include(debug_toolbar.urls)),)
