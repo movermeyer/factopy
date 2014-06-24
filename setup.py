d5fc13c7b5d86468f57a981997fbd198d86e3c85
eloy <eloy.colell@gmail.com>
2014-06-24 20:54:05 -0300

    Increment pypi version.

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ except ImportError:
 
 setup(
     name='factopy',
-    version='0.0.2',
+    version='0.0.3',
     author=u'Eloy Adonis Colell',
     author_email='eloy.colell@gmail.com',
     packages=find_packages(),
